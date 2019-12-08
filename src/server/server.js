b0911699fb220b89ef6982e1d0d0c2b619831b43
Gargarenris <19952324+sebastian-gonzalezmoran@users.noreply.github.com>
2019-12-08 20:42:04 +0100

    initial commit

---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+//Set paths
+const publicPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./views");
+
+//Set view engines
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+app.use(express.static(publicPath));
+
+//Render pages
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => {
+  console.log("Server started");
+});
